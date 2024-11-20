298e6919e2ea6cc6ad3ff417cc65b4fc446fb51f
Paul Jebb <pjebb15@gmail.com>
2024-11-20 10:42:51 +0000

    added comments model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
-from .models import Post
+from django.contrib import admin 
+from .models import Post 
 
-# Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin): 
+    list_display = ('title', 'slug', 'created_at', 'updated_on') # Include the updated_on field 
+    fields = ['title', 'slug', 'content', 'created_at', 'updated_on'] 
+    
+    admin.site.register(Post)

ea16d48c06505cc859937cb0bdff70d3a73ed44d
Paul Jebb <pjebb15@gmail.com>
2024-11-20 12:32:20 +0000

    Draft final code

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,21 @@
-from django.contrib import admin 
-from .models import Post 
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
-class PostAdmin(admin.ModelAdmin): 
-    list_display = ('title', 'slug', 'created_at', 'updated_on') # Include the updated_on field 
-    fields = ['title', 'slug', 'content', 'created_at', 'updated_on'] 
-    
-    admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+# Register your models here.
+admin.site.register(Comment)

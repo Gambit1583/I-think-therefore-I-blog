fef12ac7314e6a3118592f6fbaea7200a3f5d092
Paul Jebb <pjebb15@gmail.com>
2024-11-20 09:27:57 +0000

    Added excerpt and update_on to post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gambit1583-ithinktheref-2gghur6l8n1.ws.codeinstitute-ide.net','.herokuapp.com']
 
